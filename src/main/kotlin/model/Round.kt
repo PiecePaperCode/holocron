b9bdd25c595c11c687aef777687c57fbdeb2d59e
Marcos Gamez <dev@marcos-gamez.ch>
2022-12-31 16:25:39 +0100

    simplify swiss pairing

--- src/main/kotlin/model/Round.kt
+++ src/main/kotlin/model/Round.kt
@@ -4,6 +4,18 @@ class Round (copyPlayers: List<Player>, copyPreviousRounds: List<Round>) {
     private var players = ArrayList<Player>(copyPlayers)
     private val previousRounds = ArrayList<Round>(copyPreviousRounds)
     private val matches = ArrayList<Match>()
+    private val byes = mutableListOf<Player>()
+
+    init {
+        previousRounds.forEach {
+            it.getMatches().forEach{
+                    match ->
+                if (match.getPlayers().size == 1) {
+                    byes.add(match.getPlayers().first())
+                }
+            }
+        }
+    }
 
     fun getMatches(): List<Match> {
         return matches
@@ -13,31 +25,44 @@ class Round (copyPlayers: List<Player>, copyPreviousRounds: List<Round>) {
         return !matches.all { it.getResult() != Match.Result.ONGOING }
     }
 
-    fun pairSwiss(avoidRematches: Boolean = true) {
+    fun pairSwiss() {
         val scores = Score(previousRounds)
+        val sortedPlayers = arrayListOf<Player>()
 
-        if (previousRounds.isNotEmpty()) {
-            players = scores.generateScores()
-                .map { it.player }
-                .filter { it in players }
-            as ArrayList<Player>
-        } else {
-            players.shuffle()
+        if (previousRounds.isEmpty()){
+            sortedPlayers.addAll(players)
+            sortedPlayers.shuffle()
+        }
+        else {
+            sortedPlayers.addAll(
+                scores.generateScores()
+                    .map { it.player }
+                    .filter { it in players }
+            )
         }
 
-        for (index in 0 until players.size step 2) {
-            val player1 = players[index]
-            var player2 = if (index + 1 < players.size) players[index + 1] else null
+        // Pair players, avoiding rematches and giving byes to the lowest scoring players
+        while (sortedPlayers.isNotEmpty()) {
+            val player1 = sortedPlayers.first()
+            sortedPlayers.remove(player1)
 
-            if (avoidRematches && player2 != null) {
-                val opponents = scores.getOpponents(player1)
-                if (opponents.contains(player2)) {
-                    val availablePlayers = players.filter { it != player1 && it != player2 }
-                    player2 = availablePlayers.firstOrNull { !opponents.contains(it) }
-                }
+            val opponents = scores.getOpponents(player1)
+            val availablePlayers = sortedPlayers
+                .filter { !opponents.contains(it) }
+
+            val player2 = if (availablePlayers.isNotEmpty()) {
+                availablePlayers.first()
+            } else{
+                byes.add(player1)
+                null
+            }
+
+            if (player2 != null) {
+                sortedPlayers.remove(player2)
             }
 
             matches.add(Match(player1, player2))
         }
+
     }
 }
